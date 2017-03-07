e8381934cc6e1143887c901d404b84d431e6ba85
leviy <yaniv.levi@hpe.com>
2017-03-07 13:50:00 +0200

    Ignore tests

--- integration-tests/src/test/java/com/hp/devops/demoapp/RestServletTest.java
+++ integration-tests/src/test/java/com/hp/devops/demoapp/RestServletTest.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@Ignore
 public class RestServletTest {
 
 	private static RequestSpecification spec;
